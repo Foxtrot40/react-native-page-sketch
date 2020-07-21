77ac9ee46098e10afadbadafaa9b0983ad7a5587
Marcos da Silva Cabeceira <cabeceiramarcos@gmail.com>
2020-07-21 13:09:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container } from './App-styles';
 
-function App() {
+const App = () => {
+  const [mousePositionX, setMousePositionX] = useState();
+  const [mousePositionY, setMousePositionY] = useState();
+
+  const onMouseMove = ({ screenX, screenY }) => {
+    setMousePositionX(screenX);
+    setMousePositionY(screenY);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container onMouseMove={onMouseMove}>
+      <h1>
+        Mouse coordinates: {mousePositionX} {mousePositionY}
+      </h1>
+    </Container>
   );
-}
+};
 
 export default App;
